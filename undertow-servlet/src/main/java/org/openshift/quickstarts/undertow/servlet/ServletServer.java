c7e75562613566f6e5f79073280408264f5623ee
Deepak <deepakcdo@gmail.com>
2020-05-16 20:06:42 +0100

    Small change to change the message

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello World")
+                                    .addInitParam("message", "Hello World This is going to be a great start")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
